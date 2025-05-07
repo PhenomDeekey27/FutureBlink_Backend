0d3ef7fe9bedf92fe3ed324abf691c16a93e6daa
Kasthuri <krangan415@gmail.com>
2025-05-07 19:35:31 +0530

    removed unwanted console logs that used for testing

--- agenda/jobs/sendEmail.js
+++ agenda/jobs/sendEmail.js
@@ -3,8 +3,8 @@ const sendEmail = require("../../utils/mailer");
 module.exports = (agenda) => {
   agenda.define("send email job", async (job) => {
     const { to, subject, body } = job.attrs.data;
-    console.log(`📤 Sending email to: ${to} | Subject: ${subject}`); // Add this line
+    console.log(` Sending email to: ${to} | Subject: ${subject}`); // Add this line
     await sendEmail(to, subject, body);
-    console.log("✅ Email sent.");
+    console.log(" Email sent.");
   });
 };
--- controllers/emailController.js
+++ controllers/emailController.js
@@ -3,7 +3,6 @@ const { agenda } = require('../utils/agenda');
 const scheduleEmail = async (req, res) => {
   const { to, subject, body, delay } = req.body;
 
-  console.log(req.body,"resbody")
 
   if (!to || !subject || !body) {
     return res.status(400).json({ message: "Missing fields: to, subject, body required." });
